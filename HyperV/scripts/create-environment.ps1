40e592777e6685148ab97ef06da7c0985c2df409
Claudiu B. <bclau@users.noreply.github.com>
2015-10-14 19:06:59 +0300

    Removes unnecessary cherry-pick
    Cherry-pick related to the "resource" module import failure removed. The original patch was reverted, Thus, the cherry-pick is unnecessary.
    
    Revert patch: https://review.openstack.org/#/c/234696

--- HyperV/scripts/create-environment.ps1
+++ HyperV/scripts/create-environment.ps1
@@ -211,8 +211,6 @@ ExecRetry {
     pushd C:\OpenStack\build\openstack\nova
     git fetch https://review.openstack.org/openstack/nova refs/changes/20/213720/5
     cherry_pick FETCH_HEAD
-    git fetch https://review.openstack.org/openstack/nova refs/changes/37/234437/4
-    cherry_pick FETCH_HEAD
     & pip install -e C:\OpenStack\build\openstack\nova
     if ($LastExitCode) { Throw "Failed to install nova fom repo" }
     popd
