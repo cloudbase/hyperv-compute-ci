c26f41dce26a9188f322b64711165b387b30ef02
Octavian Ciuhandu <ociuhandu@cloudbasesolutions.com>
2015-12-11 14:53:42 +0200

    install PyMI instead of WMI

--- HyperV/scripts/create-environment.ps1
+++ HyperV/scripts/create-environment.ps1
@@ -164,7 +164,7 @@ Add-Content "$env:APPDATA\pip\pip.ini" $pip_conf_content
 
 & easy_install -U pip
 & pip install -U setuptools
-& pip install -U wmi
+& pip install -U --pre pymi
 & pip install cffi
 & pip install numpy
 popd
