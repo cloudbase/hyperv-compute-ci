c08fc4e6044449838fc174820ba40e6362dccf76
Costin Galan <cgalan@cloudbasesolutions.com>
2016-01-12 18:17:04 +0200

    Add iSCSI targets cleaning

--- jobs/run_collect_and_cleanup.sh
+++ jobs/run_collect_and_cleanup.sh
@@ -99,6 +99,11 @@ if [ "$IS_DEBUG_JOB" != "yes" ]
 		echo "Deleting devstack floating IP"
 		nova floating-ip-delete "$FLOATING_IP"
 		rm -f /home/jenkins-slave/runs/devstack_params.$ZUUL_UUID.txt
+
+		echo "Cleaning iSCSI targets"
+		python /home/jenkins-slave/tools/wsman.py -U https://$hyperv01:5986/wsman -u $WIN_USER -p $WIN_PASS 'powershell $targets = gwmi -ns root/microsoft/windows/storage -class msft_iscsitarget; $targets[0].update();' 
+		python /home/jenkins-slave/tools/wsman.py -U https://$hyperv01:5986/wsman -u $WIN_USER -p $WIN_PASS 'powershell $targets = gwmi -ns root/microsoft/windows/storage -class msft_iscsitarget; $targets[0].update();'
+
 	else
 		TIMESTAMP=$(date +%d-%m-%Y_%H-%M)
         	echo "Creating logs destination folder"
