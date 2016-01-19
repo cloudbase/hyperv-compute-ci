a12ec04a8c59727d4f63a3c4c1d6d44c62cf4144
Remus Buzatu <r.buzatu90@gmail.com>
2016-01-19 21:23:50 +0200

    Use 7z to extract python27new.tar.gz

--- devstack_vm/bin/get-isolated-tests.sh
+++ devstack_vm/bin/get-isolated-tests.sh
@@ -20,11 +20,6 @@ tests_dir=$1
 
 exclude_tests_file="/home/ubuntu/bin/excluded-tests.txt"
 isolated_tests_file="/home/ubuntu/bin/isolated-tests.txt"
-include_tests_file="/home/ubuntu/bin/included-tests.txt"
-
-if [ -f "$include_tests_file" ]; then 
-    include_tests=(`awk 'NF && $1!~/^#/' $include_tests_file`)
-fi
 
 if [ -f "$exclude_tests_file" ]; then
     exclude_tests=(`awk 'NF && $1!~/^#/' $exclude_tests_file`)
@@ -34,18 +29,13 @@ if [ -f "$isolated_tests_file" ]; then
     isolated_tests=(`awk 'NF && $1!~/^#/' $isolated_tests_file`)
 fi
 
-exclude_tests=( ${exclude_tests[@]} ${isolated_tests[@]} )
-
 exclude_regex=$(array_to_regex ${exclude_tests[@]})
-include_regex=$(array_to_regex ${include_tests[@]})
+include_regex=$(array_to_regex ${isolated_tests[@]})
 
 if [ ! "$exclude_regex" ]; then
     exclude_regex='^$'
 fi
 
 cd $tests_dir
-if [ -f "$include_tests_file" ]; then
-    testr list-tests | grep $include_regex | grep -v $exclude_regex
-else
-    testr list-tests | grep -v $exclude_regex
-fi
+testr list-tests | grep $include_regex | grep -v $exclude_regex
+
