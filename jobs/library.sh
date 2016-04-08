6eb649c1e9e14598510bba05ae2edb068e6f973d
Remus Buzatu <r.buzatu90@gmail.com>
2016-04-08 11:25:32 +0300

    Removed hard-coded paths and fine tuned the firewall

--- jobs/library.sh
+++ jobs/library.sh
@@ -36,28 +36,48 @@ run_wsmancmd_with_retry () {
     exec_with_retry "python /home/jenkins-slave/tools/wsman.py -U https://$HOST:5986/wsman -u $USERNAME -p $PASSWORD $CMD"
 }
 
-wait_for_listening_port () {
-    HOST=$1
-    PORT=$2
-    TIMEOUT=$3
+function run_wsman_cmd() {
+    local host=$1
+    local win_user=$2
+    local win_password=$3
+    local cmd=$4
+
+    python /home/jenkins-slave/tools/wsman.py -u $win_user -p $win_password -U https://$host:5986/wsman $cmd
+}
+
+function run_wsman_ps() {
+    local host=$1
+    local win_user=$2
+    local win_password=$3
+    local cmd=$4
+
+    run_wsman_cmd $host $win_user $win_password "powershell -NonInteractive -ExecutionPolicy RemoteSigned -Command $cmd"
+}
+
+function wait_for_listening_port () {
+    local HOST=$1
+    local PORT=$2
+    local TIMEOUT=$3
+
     exec_with_retry "nc -z -w$TIMEOUT $HOST $PORT" 100 5
 }
 
-run_ssh_cmd () {
-    SSHUSER_HOST=$1
-    SSHKEY=$2
-    CMD=$3
+function run_ssh_cmd () {
+    local SSHUSER_HOST=$1
+    local SSHKEY=$2
+    local CMD=$3
+
     ssh -t -o 'PasswordAuthentication no' -o 'StrictHostKeyChecking no' -o 'UserKnownHostsFile /dev/null' -i $SSHKEY $SSHUSER_HOST "$CMD"
 }
 
-run_ssh_cmd_with_retry () {
-    SSHUSER_HOST=$1
-    SSHKEY=$2
-    CMD=$3
-    INTERVAL=$4
-    MAX_RETRIES=10
+function run_ssh_cmd_with_retry () {
+    local SSHUSER_HOST=$1
+    local SSHKEY=$2
+    local CMD=$3
+    local INTERVAL=$4
+    local MAX_RETRIES=10
+    lcoal COUNTER=0
 
-    COUNTER=0
     while [ $COUNTER -lt $MAX_RETRIES ]; do
         EXIT=0
         run_ssh_cmd $SSHUSER_HOST $SSHKEY "$CMD" || EXIT=$?
@@ -73,7 +93,7 @@ run_ssh_cmd_with_retry () {
     return $EXIT
 }
 
-join_hyperv (){
+function join_hyperv (){
     run_wsmancmd_with_retry $1 $2 $3 'powershell -ExecutionPolicy RemoteSigned Remove-Item -Recurse -Force C:\OpenStack\hyperv-compute-ci ; git clone https://github.com/cloudbase/hyperv-compute-ci C:\OpenStack\hyperv-compute-ci ; cd C:\OpenStack\hyperv-compute-ci ; git checkout cambridge >>\\'$FIXED_IP'\openstack\logs\create-environment-'$1'.log 2>&1'
     run_wsmancmd_with_retry $1 $2 $3 'powershell -ExecutionPolicy RemoteSigned C:\OpenStack\hyperv-compute-ci\HyperV\scripts\teardown.ps1'
     [ "$IS_DEBUG_JOB" == "yes" ] && run_wsmancmd_with_retry $1 $2 $3 '"powershell Write-Host Calling gerrit with zuul-site='$ZUUL_SITE' gerrit-site='$ZUUL_SITE' zuul-ref='$ZUUL_REF' zuul-change='$ZUUL_CHANGE' zuul-project='$ZUUL_PROJECT' >>\\'$FIXED_IP'\openstack\logs\create-environment-'$1'.log 2>&1"'
@@ -83,16 +103,16 @@ join_hyperv (){
     run_wsmancmd_with_retry $1 $2 $3 '"powershell -ExecutionPolicy RemoteSigned C:\OpenStack\hyperv-compute-ci\HyperV\scripts\create-environment.ps1 -devstackIP '$FIXED_IP' -branchName '$ZUUL_BRANCH' -buildFor '$ZUUL_PROJECT' '$IS_DEBUG_JOB' >>\\'$FIXED_IP'\openstack\logs\create-environment-'$1'.log 2>&1"'
 }
 
-teardown_hyperv () {
+function teardown_hyperv () {
     run_wsmancmd_with_retry $1 $2 $3 'powershell -ExecutionPolicy RemoteSigned C:\OpenStack\hyperv-compute-ci\HyperV\scripts\teardown.ps1'
 }
 
-post_build_restart_hyperv_services (){
+function post_build_restart_hyperv_services (){
     run_wsmancmd_with_retry $1 $2 $3 '"powershell -ExecutionPolicy RemoteSigned C:\OpenStack\hyperv-compute-ci\HyperV\scripts\post-build-restart-services.ps1 >>\\'$FIXED_IP'\openstack\logs\create-environment-'$1'.log 2>&1"'
 }
 
-poll_shh () {
-	IP=$1
+function poll_shh () {
+	local IP=$1
 
 	if [ -z "$IP" ]
 	then
